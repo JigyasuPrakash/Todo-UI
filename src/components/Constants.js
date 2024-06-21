45b2ffae7d1957c0fbe6e7b3a00830c94b1e360a
imsrish20 <imsrish20@gmail.com>
2024-06-21 12:19:17 +0530

    changes

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,4 +1,2 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://todo-5hul.onrender.com/"
-    : "http://localhost:3030";
+export const API_URL = "https://todo-5hul.onrender.com/"
+
