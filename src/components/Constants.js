76efc841f3ad8621377a083d783886139021a8f2
imsrish20 <imsrish20@gmail.com>
2024-06-24 11:37:41 +0530

    api_url changed

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,2 +1,4 @@
-export const API_URL = "https://todo-5hul.onrender.com/"
-
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://organic-linette-just-another-project-50a7afac.koyeb.app/"
+    : "http://localhost:3030";
