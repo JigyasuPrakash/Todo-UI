3e06974128febb0c6c07ac55e08e10f0d06f07f4
JigyasuAnchanto <jigyasu.prakash@anchanto.com>
2024-06-29 15:56:14 +0530

    prod url fix

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://organic-linette-just-another-project-50a7afac.koyeb.app/"
+    ? "https://organic-linette-just-another-project-50a7afac.koyeb.app"
     : "http://localhost:3030";
